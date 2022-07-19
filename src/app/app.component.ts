01ac0c4233aa36ebd506e89631e3af50c0a29bee
Michael Leahey <michael.leahey1843@outlook.com>
2022-07-18 20:11:59 -0400

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'PasswordEx';
+  loginForm = new FormGroup({
+    uname : new FormControl('', [Validators.required,Validators.minLength(12)]),
+    pword : new FormControl('', [Validators.pattern("[0-9{3}]"),Validators.minLength(4), Validators.maxLength(10)])
+})
+onSubmit() {
+  console.log(this.loginForm.value)
+}
+get uname() {
+return this.loginForm.get('uname');
+}
+get pword() {
+return this.loginForm.get('pword');
+}
 }
