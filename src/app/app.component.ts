9ec6fca2b1ea71dc7f119a59810401f0c8276fd3
Michael Leahey <michael.leahey1843@outlook.com>
2022-07-19 09:16:56 -0400

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,18 +7,19 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'PasswordEx';
+  title = 'FormValidationExample';
+
   loginForm = new FormGroup({
-    uname : new FormControl('', [Validators.required,Validators.minLength(12)]),
-    pword : new FormControl('', [Validators.pattern("[0-9{3}]"),Validators.minLength(4), Validators.maxLength(10)])
-})
-onSubmit() {
-  console.log(this.loginForm.value)
-}
-get uname() {
-return this.loginForm.get('uname');
-}
-get pword() {
-return this.loginForm.get('pword');
-}
+      uname : new FormControl('', [Validators.required,Validators.minLength(12)]),
+      pword : new FormControl('', [Validators.pattern("[0-9{3}]"),Validators.minLength(4), Validators.maxLength(10)])
+  })
+  onSubmit() {
+      console.log(this.loginForm.value)
+  }
+  get uname() {
+    return this.loginForm.get('uname');
+  }
+  get pword() {
+    return this.loginForm.get('pword');
+  }
 }
